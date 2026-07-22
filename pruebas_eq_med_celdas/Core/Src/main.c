95c6c60a043796c82da0527356ab56463ac7ff52
GrnRant <gn.rant@gmail.com>
2026-07-22 15:51:35 -0300

    Pruebas y arreglos relacionados a compensación de lazo de tensión y mediciones de frontend de módulo de carga.

--- pruebas_eq_med_celdas/Core/Src/main.c
+++ pruebas_eq_med_celdas/Core/Src/main.c
@@ -179,11 +179,12 @@ int main(void)
 //
 //  HAL_SPI_Transmit_DMA(&hspi1, (uint8_t *) dac_buffer, sizeof(dac_buffer)/sizeof(uint16_t));
 
-  MCP4822_SetVoltage(MCP4822_CH_A ,0.1);
-  MCP4822_SetVoltage(MCP4822_CH_B , 2);
+  MCP4822_SetVoltage(MCP4822_CH_A , 2.0);
+  MCP4822_SetVoltage(MCP4822_CH_B , 1.7);
   HAL_Delay(100);
-//  MCP4822_FillSineDMA(dac_buffer, sizeof(dac_buffer)/sizeof(uint16_t), 0.07f, 1.0f, 1, MCP4822_CH_A);
-  HAL_Delay(5000);
+  //MCP4822_FillSineDMA(dac_buffer, sizeof(dac_buffer)/sizeof(uint16_t), 0.1f, 1.0f, 1, MCP4822_CH_A);
+  while(HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin));
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -191,7 +192,10 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+//	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+//	  HAL_Delay(2000);
+//	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
+//	  HAL_Delay(2000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -400,11 +404,21 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_RESET);
+
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
